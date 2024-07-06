507c2ae8522c3cdb5b2ea942155b7fc32c7d00c3
Sukanya1268 <sukanyadussani@gmail.com>
2024-07-06 21:47:54 +0530

    Add files via upload

---
+++ UserDashboard.js
@@ -0,0 +1,10 @@
+import Dashboard from './Dashboard';
+import { Component } from 'react';
+class UserDashboard extends Component{
+    render(){
+        return <h1>Welcome User</h1>
+    }
+
+}
+export default UserDashboard;
+
