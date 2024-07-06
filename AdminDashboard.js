ce38340c15a02900a78bf2c625eb1217d6380e5c
Sukanya1268 <sukanyadussani@gmail.com>
2024-07-06 21:45:59 +0530

    Add files via upload

---
+++ AdminDashboard.js
@@ -0,0 +1,9 @@
+import Dashboard from "./Dashboard";
+import { Component } from 'react';
+class AdminDashboard extends Component{
+    render(){
+        return <h1>Welcome Admin</h1>
+    }
+
+}
+export default AdminDashboard;
